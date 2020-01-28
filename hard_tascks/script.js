1a2f3fec0f52d101fcf4a0da6af6948f3c0038c6
Roman Chagovtsev <4agovtsev@gmail.com>
2020-01-28 13:09:08 +0100

    Решил трудные задачи к третьему уроку, тернарный оператор ?

--- hard_tascks/script.js
+++ hard_tascks/script.js
@@ -1,12 +1,41 @@
-var num = '266219';
+'use strict'
 
-arr = num.split('');
+let lang = 'ru';
+let result1;
+let result2;
+let langArr;
+let namePerson = 'Роман';
+
+if (lang === 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sanday')
+}
+
+switch (lang) {
+    case 'ru': 
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break;
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sanday')
+        break;
+    default: 
+        console.log('Что-то не то')
+}
+
+langArr = [
+    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sanday']
+];
+
+result1 = (lang === 'ru') ? langArr[0] : 
+         (lang ==='en') ? langArr[1] : 
+         'Пустая строка';
+console.log('result1: ', result1);
+
+result2 = (namePerson === 'Артем') ? 'директор' :
+          (namePerson === 'Максим') ?'преподаватель' :
+          'студент';
+console.log('result2: ', result2);
 
 
-var result = 1;
-for(i = 0; i < arr.length; i++){
-    result *= arr[i]; 
-   
-};
-
-console.log(((result ** 3) + '').substring(0, 2))
